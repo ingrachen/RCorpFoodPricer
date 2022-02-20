32e479b2e7d6cddd68849dea95d18b2ea2a72bf1
sadia ingrachen <ingrachen.sadia@gmail.com>
2022-02-20 20:16:57 +0100

    first commit

---
+++ RCorpFoodPricer/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace RCorpFoodPricer
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //pour tester, lancer en ligne de commande : 
+            //dotnet run -- "assiette" "couscous" "coca" "moyen" "baba" "normal" "yes"
+            var price = new Pricing().Compute(args[0],args[1],args[2],args[3], args[4], args[5], args[6]);
+            Console.WriteLine($"Prix à payer : {price}€");
+        }
+    }
+}
